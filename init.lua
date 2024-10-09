545ca1be1fca4106588dbb40333b89e30c65f790
Matthew Sumpter <matthew.sumpter@meowwolf.com>
2024-10-09 11:21:31 -0600

    Add neogit

--- init.lua
+++ init.lua
@@ -454,6 +454,19 @@ require('lazy').setup({
       require('nvim-tree').setup {}
     end,
   },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed.
+      'nvim-telescope/telescope.nvim', -- optional
+      'ibhagwan/fzf-lua', -- optional
+      'echasnovski/mini.pick', -- optional
+    },
+    config = true,
+  },
 
   -- LSP Plugins
   {
