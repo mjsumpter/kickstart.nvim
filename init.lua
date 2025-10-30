927a07b3749ba9b06b3035879ca7aa104326cd32
mjsumpter <github@mjs.simplelogin.com>
2025-10-30 12:42:16 -0600

    add golang to nvim

--- init.lua
+++ init.lua
@@ -637,7 +637,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -745,6 +745,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         rust = { 'rustfmt', lsp_format = 'always' },
+        go = { 'gofmt' },
         javascript = { 'prettier' },
         typescript = { 'prettier' },
       },
